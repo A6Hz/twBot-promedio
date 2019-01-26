c9d5536158c679a04cf19d8f8594b0204e756bc3
Alex143 <full-back-1996-05@hotmail.com>
2019-01-26 13:28:04 -0400

    init

--- server.py
+++ server.py
@@ -2,4 +2,11 @@ from os import environ
 from flask import Flask
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+    
+
 app.run(host= '0.0.0.0', port=environ.get('PORT'))
